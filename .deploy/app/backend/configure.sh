0797c839cb198e453d50672c4aba921b10d4777f
Cesar <cesargb@gmail.com>
2021-07-25 16:30:26 +0200

    add .env fix

--- .deploy/app/backend/configure.sh
+++ .deploy/app/backend/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH_DEPLOY="/opt/codedeploy-agent/deployment-root/{$DEPLOYMENT_GROUP_ID}/{$DEPLOYMENT_ID}/deployment-archive/"
+PATH_DEPLOY="/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/"
 PATH_ROOT="/var/www/html/backend"
 REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 
@@ -10,5 +10,5 @@ cd $PATH_ROOT
 sudo composer install
 
 # Env
-APP_KEY=$(aws ssm get-parameters --region {$REGION} --names app.backend.key --with-decryption --query Parameters[0].Value)
-cat "{$PATH_DEPLOY}/.deploy/app/backend/.env" | sed -e "s/APP_KEY=/APP_KEY={$APP_KEY}/" > "{$PATH_ROOT}/.env"
+APP_KEY=$(aws ssm get-parameters --region ${REGION} --names app.backend.key --with-decryption --query Parameters[0].Value)
+cat "${PATH_DEPLOY}/.deploy/app/backend/.env" | sed -e "s/APP_KEY=/APP_KEY=${APP_KEY}/" > "${PATH_ROOT}/.env"
