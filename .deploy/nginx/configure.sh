6995b4df9b620e7c1314e2ede82a2a574fa1b358
Cesar <cesargb@gmail.com>
2021-07-25 15:23:32 +0200

    add nginx

--- .deploy/nginx/configure.sh
+++ .deploy/nginx/configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cp $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/.deploy/nginx/default.conf /etc/nginx/conf.d/
+sudo cp /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/.deploy/nginx/default.conf /etc/nginx/conf.d/
