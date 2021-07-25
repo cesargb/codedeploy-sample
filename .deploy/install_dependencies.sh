0dfec14bfa98ae47d38a409f3a75679a17b509ac
Cesar <cesargb@gmail.com>
2021-07-25 13:36:57 +0200

    add code deploy first

---
+++ .deploy/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo amazon-linux-extras install php8.0
+sudo yum install -y httpd mariadb-server php
---
+++ .deploy/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+systemctl start mariadb.service
+systemctl start httpd.service
+systemctl start php-fpm.service
