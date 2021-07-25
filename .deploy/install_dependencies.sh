eff143358edfb2ec4befc0606dd57ee4a20056dc
Cesar <cesargb@gmail.com>
2021-07-25 14:28:48 +0200

    add composer

--- .deploy/install_dependencies.sh
+++ .deploy/install_dependencies.sh
@@ -2,6 +2,3 @@
 
 sudo amazon-linux-extras install php8.0
 sudo yum install -y httpd mariadb-server php
-sudo curl -sS https://getcomposer.org/installer | sudo php
-sudo mv composer.phar /usr/local/bin/composer
-sudo ln -s /usr/local/bin/composer /usr/bin/composer
